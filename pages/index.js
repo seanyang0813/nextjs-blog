deb4ffda6b0aa8055d19b08037027799bda4dda0
Sean Yang <seanyang0813@gmail.com>
2021-01-15 16:51:24 -0500

    update order

--- pages/index.js
+++ pages/index.js
@@ -31,11 +31,13 @@ export default function Home({ allPostsData }) {
         </div>
         <div className="flex justify-center">
           <div className="grid grid-cols-1 md:grid-cols-2">
-              <Card content={{image:"/images/code-documentation.png",title:"Code Documentation Tool", link:"https://github.com/seanyang0813/code-documentation", width: 800, height: 400}}>A tool to create sql templates with documentations. It has a built in editor and allows code insertion into the editor directly. It aims to lower the barrier to use code for non technical people</Card>
-              <Card content={{image:"/images/back-of-zoom.png", title:"Back of Zoom", link:"https://github.com/seanyang0813/backofzoom", width: 400, height: 400}}>A real time chat app that lets student enter the zoom url they are in to chat with others in the meeting without distracting other in the actual zoom chat</Card>
-              <Card content={{image:"/images/page-flip.png",title:"Flip Pages with Facial Expression", link:"https://github.com/seanyang0813/facial-expression-keyboard-action-simulator", width: 600, height: 400}}>A desktop app that simulates keyboard actions based on facial expression to flip pages on e-books</Card>
-              <Card content={{image:"/images/common.png",title:"Common.gg", link:"https://common.gg/", width: 400, height: 400}}>I am on the Engineering team at common.gg, a social media network focused on the game teamfight tactics</Card>
-            </div>
+            <Card content={{image:"/images/back-of-zoom.png", title:"Back of Zoom", link:"https://github.com/seanyang0813/backofzoom", width: 400, height: 400}}>A real time chat app that lets student enter the zoom url they are in to chat with others in the meeting without distracting other in the actual zoom chat</Card>
+            <Card content={{image:"/images/common.png",title:"Common.gg", link:"https://common.gg/", width: 400, height: 400}}>I am on the Engineering team at common.gg, a social media network focused on the game teamfight tactics</Card>
+            <Card content={{image:"/images/code-documentation.png",title:"Code Documentation Tool", link:"https://github.com/seanyang0813/code-documentation", width: 800, height: 400}}>A tool to create sql templates with documentations. It has a built in editor and allows code insertion into the editor directly. It aims to lower the barrier to use code for non technical people</Card>
+            
+            <Card content={{image:"/images/page-flip.png",title:"Flip Pages with Facial Expression", link:"https://github.com/seanyang0813/facial-expression-keyboard-action-simulator", width: 600, height: 400}}>A desktop app that simulates keyboard actions based on facial expression to flip pages on e-books</Card>
+            
+          </div>
         </div>
         
       </section>
